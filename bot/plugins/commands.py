16d9db329eb5479f734e9c97f4e8744fc779dc76
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-04-25 14:18:32 +0700

    Add files via upload

---
+++ bot/plugins/commands.py
@@ -0,0 +1,23 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from messages import Message
+
+@Client.on_message(filters.command(["start"]))
+async def start(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=Message.START_TEXT.format(message.from_user.mention),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=message.message_id
+    )
+
+@Client.on_message(filters.command(["help"]))
+async def help(client, message):
+    await client.send_message(
+        chat_id=message.chat.id,
+        text=Message.HELP_USER,
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=message.message_id
+    )
---
+++ bot/plugins/qr_encode.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# This is bot coded by Abhijith-cloud and used for educational purposes only
+# https://github.com/Abhijith-cloud
+# (c) Abhijith N T ;-)
+# Thank you https://github.com/pyrogram/pyrogram :-)
+
+
+import os
+from pyrogram import Client, filters 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import pyqrcode
+from messages import Message
+from bot.plugins.display.display_progress import progress
+
+
+@Client.on_message(filters.text & filters.private)
+async def qr_encode(client, message):
+    qr = await client.send_message(
+        chat_id=message.chat.id,
+        text="Making your QR Code...",
+        reply_to_message_id=message.message_id
+    )
+    s = str(message.text)
+    qrname = str(message.from_user.id)
+    qrcode = pyqrcode.create(s)
+    qrcode.png(qrname + '.png', scale=6)
+    img = qrname + '.png'
+    try:
+        response = upload_file(img)
+    except Exception as error:
+        await qr.edit_text(f"{Message.error}")
+        return
+    try:
+        await message.reply_photo(
+            photo=img,
+            progress=progress,
+            progress_args=("Trying to Uploading....", qr)
+        )
+
+    except Exception as error:
+        print(error)
+    try:
+        os.remove(img)
+    except Exception as error:
+        print('Something is {error}')
